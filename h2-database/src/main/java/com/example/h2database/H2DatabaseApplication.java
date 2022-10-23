658d5aac403d43144fb0756ec8770fa1aff43e68
Bryan Cusme <bryan.cusme@devoteam.com>
2022-10-23 21:27:33 +0200

    add propperties to set up the h2 DB

--- h2-database/src/main/java/com/example/h2database/H2DatabaseApplication.java
+++ h2-database/src/main/java/com/example/h2database/H2DatabaseApplication.java
@@ -2,12 +2,15 @@ package com.example.h2database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class H2DatabaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(H2DatabaseApplication.class, args);
+
+        ConfigurableApplicationContext context= SpringApplication.run(H2DatabaseApplication.class, args);
+
     }
 
 }
