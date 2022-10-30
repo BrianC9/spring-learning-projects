c7cc70ff2c9061fc02d61e0d35cb880087631fc5
Bryan Cusme <bryan.cusme@devoteam.com>
2022-10-30 13:08:01 +0100

    add security config for jwt with asymetrical keys

---
+++ spring-security-jwt/src/main/java/com/example/springsecurityjwt/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.springsecurityjwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails bryanUser = User.withDefaultPasswordEncoder()
+                .username("Bryan")
+                .password("password")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(bryanUser);
+    }
+}
